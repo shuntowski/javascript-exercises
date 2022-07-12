7f4d04d96888e213a8448c90f0bd91d737dbe2ac
shuntowski <Samuel.Huntowski@gmail.com>
2022-07-11 21:34:01 -0500

    Finish exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    //take string
+    //slice off first character, put it on the end
+    //repeat it until all the characters have been dealt with
 
+    let result = '';
+
+    for(let i = 0; i < input.length; i++){
+        let nextChar = input.charAt(input.length-i-1);
+        result = result + nextChar;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
