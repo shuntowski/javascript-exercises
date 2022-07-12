320849dc76a4d1b9e513d8ce54f532e2acbd29d3
shuntowski <Samuel.Huntowski@gmail.com>
2022-07-11 20:21:35 -0500

    Finish test 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(input, numRepetitions) {
+    let result = '';
 
+    if (numRepetitions < 0){
+        return 'ERROR';
+    }
+
+    for (let i = 0; i < numRepetitions; i++){
+        result = result + input;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
