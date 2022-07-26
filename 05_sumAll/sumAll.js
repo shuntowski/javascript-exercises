c9dabbf9275b87314cf620163afbf6a4d822633b
shuntowski <Samuel.Huntowski@gmail.com>
2022-07-26 18:17:09 -0500

    Finish JS excercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    let sum = 0;
 
+    if ((int1 < 0) || (int2 < 0) || (typeof int1 != "number") || 
+    (typeof int2 != "number")){
+        sum = 'ERROR';
+        return sum;
+    }
+
+    if ((int1 <= int2) == true){
+        for (let i = int1; i <= int2; i++){
+            sum += i;
+        }
+    } else {
+        for (let i = int2; i <= int1; i++){
+            sum += i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
