b1354b6d5905bedcb04fd8665aaa1fc823acef76
shuntowski <Samuel.Huntowski@gmail.com>
2022-07-26 17:31:55 -0500

    implement first real attempt at this algo

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+    let returnArray = new Array();
 
+    for(let i = 0; i < array.length; i++){
+        if (elements.find(array[i]) == undefined){
+            returnArray.push(array[i]);
+        }
+    }
+
+    return returnArray;
 };
 
 // Do not edit below this line
