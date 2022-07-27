0237d298b0945b492e837c7a41ac72931c58c4b4
shuntowski <Samuel.Huntowski@gmail.com>
2022-07-26 21:34:50 -0500

    Finish JS excercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  return ((Math.round((fTemp - 32) * (5/9)) * 10) / 10)
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  return (Math.round(((cTemp * 1.8) + 32) * 10) / 10)
 };
 
 // Do not edit below this line
